93ec91adc96d559074d7f8cea42b9d11f8a26ec6
Terence Chiu <tchiu2@gmail.com>
2019-07-11 10:09:53 -0400

    update server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const apiRouter = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use('/api', apiRouter);
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
