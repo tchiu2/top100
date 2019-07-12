b48455a46c1e6bfc939f72282f1c142f3f723560
Terence Chiu <tchiu2@gmail.com>
2019-07-12 01:27:20 -0400

    add sorting to table columns

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {
+  Menu,
   Popover,
   Position,
   Table,
+  TextDropdownButton,
 } from 'evergreen-ui';
 
 import Notes from './Notes';
@@ -15,33 +17,28 @@ const CELL_SIZES = {
 };
 
 const COLUMNS = [
-  { name: 'rank', size: 'small' },
-  { name: 'winery', size: 'medium', align: 'left' },
-  { name: 'wine', size: 'large', align: 'left' },
-  { name: 'vintage', size: 'small' },
-  { name: 'score', size: 'small' },
-  { name: 'color', size: 'small' },
-  { name: 'country', size: 'medium', align: 'left' },
-  { name: 'region', size: 'medium', align: 'left' },
+  { index: 1, name: 'rank', size: 'small', sortable: true },
+  { index: 2, name: 'winery', size: 'medium', align: 'left' },
+  { index: 3, name: 'wine', size: 'large', align: 'left' },
+  { index: 4, name: 'vintage', size: 'small' },
+  { index: 5, name: 'score', size: 'small', sortable: true },
+  { index: 6, name: 'color', size: 'small' },
+  { index: 7, name: 'country', size: 'medium', align: 'left' },
+  { index: 8, name: 'region', size: 'medium', align: 'left' },
 ];
 
-const tableCell = (isHeader, name, size, textAlign = 'center') => {
-  const Cell = isHeader ? Table.TextHeaderCell : Table.TextCell;
-
-  return (
-    <Cell
-      flex={CELL_SIZES[size]}
-      textAlign={textAlign}
-    >
-      {isHeader ? name[0].toUpperCase() + name.slice(1) : name}
-    </Cell>
-  );
+const ORDER = {
+  NONE: 'NONE',
+  ASC: 'ASC',
+  DESC: 'DESC',
 };
 
 class List extends Component {
   state = {
     wines: [],
     shown: null,
+    orderedColumn: 1,
+    ordering: ORDER.NONE,
   };
 
   componentDidMount() {
@@ -52,6 +49,25 @@ class List extends Component {
     fetch('/api/wines')
       .then(res => res.json())
       .then(wines => this.setState({ wines }));
+  };
+
+  sort = wines => {
+    const { ordering, orderedColumn } = this.state;
+
+    if (ordering === ORDER.NONE) return wines;
+
+    const key = COLUMNS[orderedColumn - 1].name;
+
+    return wines.sort((a, b) => {
+      const aVal = a[key];
+      const bVal = b[key];
+
+      const sortTable = { true: 1, false: -1 };
+
+      if (ordering === ORDER.ASC) return aVal === bVal ? 0 : sortTable[aVal > bVal];
+
+      return bVal === aVal ? 0 : sortTable[bVal > aVal];
+    });
   };
 
   debouncedMouseOver = debounce(id => {
@@ -74,12 +90,59 @@ class List extends Component {
     this.throttledScroll();
   };
 
+  createTableCell = (isHeader, name, size, textAlign = 'center', index, isSortable = true) => {
+    const Cell = isHeader ? Table.TextHeaderCell : Table.TextCell;
+
+    return (
+      <Cell
+        flex={CELL_SIZES[size]}
+        textAlign={textAlign}
+      >
+        {!isHeader
+          ? name
+          : isSortable
+            ? <Popover
+                position={Position.BOTTOM_LEFT}
+                content={({ close }) => (
+                  <Menu>
+                    <Menu.OptionsGroup
+                      title="Order"
+                      options={[
+                        { label: 'Ascending', value: ORDER.ASC },
+                        { label: 'Descending', value: ORDER.DESC },
+                      ]}
+                     selected={this.state.orderedColumn === index ? this.state.ordering : null}
+                     onChange={value => {
+                       this.setState({
+                         orderedColumn: index,
+                         ordering: value,
+                       });
+
+                       close();
+                     }}
+                    />
+                  </Menu>
+                )}
+              >
+                <TextDropdownButton
+                >
+                  {name[0].toUpperCase() + name.slice(1)}
+                </TextDropdownButton>
+              </Popover>
+            : name[0].toUpperCase() + name.slice(1)
+        }
+      </Cell>
+    );
+  };
+
   renderHeaders = () =>
     <Table.Head>
-      {COLUMNS.map(({ name, size, align }) => tableCell(true, name, size, align))}
+      {COLUMNS.map(({ name, size, align, index, sortable }) =>
+        this.createTableCell(true, name, size, align, index, sortable)
+      )}
     </Table.Head>
 
-  renderTableRows = () => this.state.wines.map(wine => (
+  renderTableRows = () => this.sort(this.state.wines).map(wine => (
     <Popover
       isShown={wine.id === this.state.shown}
       key={wine.id}
@@ -93,7 +156,7 @@ class List extends Component {
         onMouseOver={this.handleMouseOver(wine.id)}
         onMouseLeave={this.handleMouseLeave}
       >
-        {COLUMNS.map(({ name, size, align }) => tableCell(false, wine[name], size, align))}
+        {COLUMNS.map(({ name, size, align }) => this.createTableCell(false, wine[name], size, align))}
       </Table.Row>
     </Popover>
   ));
